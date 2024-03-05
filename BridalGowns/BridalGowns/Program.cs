0b1a54c2ecfb63753078d22d468566b53e852c41
גרוסנס חיה < @mbjcomp.local>
2024-03-05 13:50:52 +0200

    merge program file

--- BridalGowns/BridalGowns/Program.cs
+++ BridalGowns/BridalGowns/Program.cs
@@ -3,20 +3,13 @@ using DAL.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-<<<<<<< HEAD
-=======
 
->>>>>>> aa828caf716806d70ffc9a66d99ce8e700724458
 
 
 DBActions actions = new DBActions(builder.Configuration);
 var connString = actions.GetConnectionString("BridalGownDB");
 builder.Services.AddDbContext<BridalContext>(options => options.UseSqlServer(connString));
 var app = builder.Build();
-<<<<<<< HEAD
-
-=======
->>>>>>> aa828caf716806d70ffc9a66d99ce8e700724458
 
 app.MapGet("/", () => "Hello World!");
 
